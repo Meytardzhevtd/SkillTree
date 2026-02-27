fd2c2a64092bcef9cbc83f30923cc1c6da2fd1ae
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-02-27 17:33:44 +0300

    Create courses table and repository

---
+++ backend/src/main/java/com/skilltree/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.skilltree.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.skilltree.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+	List<Course> findByUserId(Long userId);
+}
