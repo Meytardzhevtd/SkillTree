0f03899e9303809bbc7fdd7f5527d941af203fcf
Arki <gameak8800@gmail.com>
2026-02-28 03:17:44 +0300

    formatting

--- backend/src/main/java/com/skilltree/dto/CreateModuleRequest.java
+++ backend/src/main/java/com/skilltree/dto/CreateModuleRequest.java
@@ -3,20 +3,20 @@ package com.skilltree.dto;
 import java.util.List;
 
 public class CreateModuleRequest {
-    private String name;
-    private List<CreateTaskRequest> tasks;
+	private String name;
+	private List<CreateTaskRequest> tasks;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public List<CreateTaskRequest> getTasks() {
-        return tasks;
-    }
-    public void setTasks(List<CreateTaskRequest> tasks) {
-        this.tasks = tasks;
-    }
+	public List<CreateTaskRequest> getTasks() {
+		return tasks;
+	}
+	public void setTasks(List<CreateTaskRequest> tasks) {
+		this.tasks = tasks;
+	}
 }
--- backend/src/main/java/com/skilltree/model/Task.java
+++ backend/src/main/java/com/skilltree/model/Task.java
@@ -4,16 +4,16 @@ import jakarta.persistence.*;
 
 @Entity
 public class Task {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "id_module", nullable = false)
-    private Module module;
+	@ManyToOne
+	@JoinColumn(name = "id_module", nullable = false)
+	private Module module;
 
-    @Column(nullable = false)
-    private String content; // only text now!!!
+	@Column(nullable = false)
+	private String content; // only text now!!!
 
-    // add getters!!!
+	// add getters!!!
 }
