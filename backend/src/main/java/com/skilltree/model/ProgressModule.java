4b48bbcd44e67e62a5738714fef266ec8e3fa518
Matvey Bazhenov <bazhenov.matvej@bk.ru>
2026-03-01 22:23:03 +0300

    New tables

---
+++ backend/src/main/java/com/skilltree/model/ProgressModule.java
@@ -0,0 +1,43 @@
+package com.skilltree.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ProgressModule",
+		uniqueConstraints = @UniqueConstraint(name = "uq_progress",
+				columnNames = {"id_module", "id_taken_course"}))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgressModule {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "id_module", referencedColumnName = "id", nullable = false,
+			foreignKey = @ForeignKey(name = "fk_progress_module",
+					value = ConstraintMode.CONSTRAINT))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Module module;
+
+	@ManyToOne
+	@JoinColumn(name = "id_taken_course", referencedColumnName = "id", nullable = false,
+			foreignKey = @ForeignKey(name = "fk_progress_taken_course",
+					value = ConstraintMode.CONSTRAINT))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private TakenCourses taken_courses;
+
+	@Column(name = "progress", nullable = false)
+	private Float progress;
+
+	@OneToMany(mappedBy = "progress_module", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<UserAnswers> user_answers = new ArrayList<>();
+
+}
---
+++ backend/src/main/java/com/skilltree/model/Roles.java
@@ -0,0 +1,35 @@
+package com.skilltree.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+@Entity
+@Table(name = "Roles",
+		uniqueConstraints = @UniqueConstraint(name = "uq_role",
+				columnNames = {"id_course", "id_user"}))
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Roles {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "course_role", nullable = false)
+	private String course_role;
+
+	@ManyToOne
+	@JoinColumn(name = "id_user", referencedColumnName = "id", nullable = false,
+			foreignKey = @ForeignKey(name = "fk_role_user", value = ConstraintMode.CONSTRAINT))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Users user;
+
+	@ManyToOne
+	@JoinColumn(name = "id_course", referencedColumnName = "id", nullable = false,
+			foreignKey = @ForeignKey(name = "fk_role_course", value = ConstraintMode.CONSTRAINT))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Courses course;
+
+}
