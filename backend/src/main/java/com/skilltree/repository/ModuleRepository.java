a97e231e201592f6db58060970a87a4fc98dfa88
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-04 15:18:07 +0300

    formatting

--- backend/src/main/java/com/skilltree/repository/ModuleRepository.java
+++ backend/src/main/java/com/skilltree/repository/ModuleRepository.java
@@ -6,8 +6,8 @@ import com.skilltree.model.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Репозиторий для Module.
- * Добавлены методы для удобного получения модулей по курсу.
+ * Репозиторий для Module. Добавлены методы для удобного получения модулей по
+ * курсу.
  */
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 
@@ -16,7 +16,8 @@ public interface ModuleRepository extends JpaRepository<Module, Long> {
 	/**
 	 * Получить все модули курса по id курса, упорядоченные по id.
 	 *
-	 * @param courseId id курса (Module.course.id)
+	 * @param courseId
+	 *            id курса (Module.course.id)
 	 * @return список модулей курса
 	 */
 	List<Module> findByCourseIdOrderById(Long courseId);
