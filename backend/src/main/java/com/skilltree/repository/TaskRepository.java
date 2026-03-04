d8c941343baf114516abed56abb840f7967e61e4
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-04 18:23:44 +0300

    add findByModul

--- backend/src/main/java/com/skilltree/repository/TaskRepository.java
+++ backend/src/main/java/com/skilltree/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.skilltree.repository;
 
 import com.skilltree.model.Task;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByModule(Module module);
 }
