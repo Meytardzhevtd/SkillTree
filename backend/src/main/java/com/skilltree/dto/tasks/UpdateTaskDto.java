2f7c1d2c3f5e38b6ad0c49b3c84e3ca27fb5d5cf
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-04 20:25:13 +0300

    upd: add validation (@NotNull and @Size)

--- backend/src/main/java/com/skilltree/dto/tasks/UpdateTaskDto.java
+++ backend/src/main/java/com/skilltree/dto/tasks/UpdateTaskDto.java
@@ -2,6 +2,8 @@ package com.skilltree.dto.tasks;
 
 import java.util.Map;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +11,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateTaskDto {
+    @NotNull
     private Long id;
+
+    @NotNull
     private Long taskTypeId;
+
+    @NotNull
+    @Size(min = 1)
     private Map<String, Object> content;
 }
