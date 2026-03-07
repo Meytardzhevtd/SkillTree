04100d9dcd35e80efe95059883ccf2365012d370
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-07 15:03:48 +0300

    fix: add query to findByUserId

--- backend/src/main/java/com/skilltree/repository/CourseRepository.java
+++ backend/src/main/java/com/skilltree/repository/CourseRepository.java
@@ -2,11 +2,13 @@ package com.skilltree.repository;
 
 import com.skilltree.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Courses, Long> {
+	@Query("SELECT c FROM Courses c JOIN c.takenCourses tc WHERE tc.user.id = :userId")
 	List<Courses> findByUserId(Long userId);
 }
