83bde5d6d421e18b407c4dc0fe260248438ea4eb
Matvey Bazhenov <bazhenov.matvej@bk.ru>
2026-03-17 23:31:12 +0300

    Mockito

--- backend/src/test/java/com/skilltree/controller/TaskControllerTest.java
+++ backend/src/test/java/com/skilltree/controller/TaskControllerTest.java
@@ -21,8 +21,8 @@ import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,10 +34,10 @@ public class TaskControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@MockBean
+	@MockitoBean
 	private TaskService taskService;
 
-	@MockBean
+	@MockitoBean
 	private com.skilltree.Service.JwtService jwtService;
 
 	@Test
