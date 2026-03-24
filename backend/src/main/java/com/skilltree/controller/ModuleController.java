6bb17dbe29a9d9adc7c2e422dc557dd309d22fd8
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-24 23:39:31 +0300

    dont work

--- backend/src/main/java/com/skilltree/controller/ModuleController.java
+++ backend/src/main/java/com/skilltree/controller/ModuleController.java
@@ -3,9 +3,13 @@ package com.skilltree.controller;
 import com.skilltree.Service.CourseService;
 import com.skilltree.Service.ModuleService;
 import com.skilltree.dto.module.ModuleDto;
+import com.skilltree.dto.module.ModuleResponse;
 import com.skilltree.dto.courses.CourseDto;
 import com.skilltree.dto.courses.CreateCourseRequest;
 import com.skilltree.dto.module.CreateModuleRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +25,13 @@ public class ModuleController {
 	}
 
 	@PostMapping
-	public ModuleDto create(@RequestBody CreateModuleRequest request) {
+	public ModuleResponse create(@RequestBody CreateModuleRequest request) {
 		return moduleService.createModule(request);
 	}
+
+	@GetMapping("/{id}")
+	public ModuleResponse getModule(@PathVariable Long id) {
+		return moduleService.getModule(id);
+	}
+	
 }
