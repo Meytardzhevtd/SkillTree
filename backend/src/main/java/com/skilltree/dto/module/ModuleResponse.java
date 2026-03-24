ad414d73334af397c925acc501314abd8a95fc98
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-24 23:38:27 +0300

    add dto

---
+++ backend/src/main/java/com/skilltree/dto/module/ModuleResponse.java
@@ -0,0 +1,16 @@
+package com.skilltree.dto.module;
+
+import java.util.List;
+
+import com.skilltree.dto.tasks.TaskSimpleDto;
+
+/**
+ * Этот dto возвращается при GET-запросе по id модуля. Важно, что если этот dto
+ * возвращается, то гарантируется, что модуль доступен. Когда мы возращаем
+ * ModuleResponse. Мы не видим содержимое тасок. Мы видим список тасок. Можно
+ * кликнуть по кнопочке таски и откроется страничка с самой задачей.
+ *
+ */
+
+public record ModuleResponse(Long moduleId, String name, List<TaskSimpleDto> tasks) {
+}
