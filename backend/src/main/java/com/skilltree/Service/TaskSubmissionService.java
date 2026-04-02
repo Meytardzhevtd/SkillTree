2fbd4bc973f3f0e1feca03bfdd004b3b14531f86
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-04-02 21:40:39 +0300

    fix

--- backend/src/main/java/com/skilltree/Service/TaskSubmissionService.java
+++ backend/src/main/java/com/skilltree/Service/TaskSubmissionService.java
@@ -1,39 +1,8 @@
 package com.skilltree.Service;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
-
-import com.skilltree.dto.tasks.SubmitTaskRequest;
-import com.skilltree.dto.tasks.TaskSubmissionResponse;
-import com.skilltree.exception.TaskNotFoundException;
-import com.skilltree.model.Task;
-import com.skilltree.model.TaskTypes;
-import com.skilltree.repository.TaskRepository;
 
 @Service
 public class TaskSubmissionService {
-	private final TaskRepository taskRepository;
 
-	public TaskSubmissionService(TaskRepository taskRepository) {
-		this.taskRepository = taskRepository;
-	}
-
-	public TaskSubmissionResponse submitTask(SubmitTaskRequest request) {
-		Task task = taskRepository.findById(request.taskId())
-				.orElseThrow(() -> new TaskNotFoundException(request.taskId()));
-
-		String taskTypeName = task.getTask_type().getName();
-
-		switch (taskTypeName) {
-			case "ONE_POSSIBLE_ANSWER" :
-
-				break;
-
-			default :
-				break;
-		}
-
-		return null;
-	}
 }
--- backend/src/main/java/com/skilltree/dto/tasks/SubmitTaskRequest.java
+++
@@ -1,6 +0,0 @@
-package com.skilltree.dto.tasks;
-
-import java.util.Map;
-
-public record SubmitTaskRequest(Long taskId, Map<String, Object> answer) {
-}
