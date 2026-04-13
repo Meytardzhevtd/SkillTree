2eb950132cd0f9af544175dd6a0c685847291eb7
Matvey Bazhenov <bazhenov.matvej@bk.ru>
2026-04-13 19:27:51 +0300

    fix make local variable

--- backend/src/main/java/com/skilltree/Service/DependenciesService.java
+++ backend/src/main/java/com/skilltree/Service/DependenciesService.java
@@ -19,7 +19,6 @@ public class DependenciesService {
 	private final ModuleRepository moduleRepository;
 	private final DependencyRepository dependencyRepository;
 	private final ProgressModuleRepository progressModuleRepository;
-	private final HashMap<Long, List<Long>> graph = new HashMap<>();
 
 	public DependenciesService(ModuleRepository moduleRepository,
 			DependencyRepository dependencyRepository,
@@ -29,40 +28,45 @@ public class DependenciesService {
 		this.progressModuleRepository = progressModuleRepository;
 	}
 
-	private void dfs(Long node) {
+	private void dfs(Long node, HashMap<Long, List<Long>> graph) {
 		// граф должен быть деревом, поэтому без проверки на то, что
 		// снова зайдем в одну и ту же вершину во время обхода
 		List<Long> list = dependencyRepository.findByModuleId(node).stream()
 				.map(dependencies -> dependencies.getBlock_module().getId()).toList();
 		graph.put(node, list);
 		for (Long next : list) {
-			dfs(next);
+			dfs(next, graph);
 		}
 	}
 
-	private void makeGraph(Long root) {
-		graph.clear();
-		dfs(root);
+	private HashMap<Long, List<Long>> makeGraph(Long root) {
+		HashMap<Long, List<Long>> graph = new HashMap<>();
+		dfs(root, graph);
+		return graph;
 	}
 
-	private boolean checkForCycles(Long node, Long neverReach) {
+	private boolean checkForCycles(Long node, Long neverReach, HashMap<Long, List<Long>> graph) {
 		boolean returnValue = false;
-		for (Long next : graph.get(node)) {
-			if (next.equals(neverReach)) {
-				return true;
-			}
-			returnValue = checkForCycles(next, neverReach);
-			if (returnValue) {
-				break;
+		if (graph.get(node) != null) {
+			for (Long next : graph.get(node)) {
+				if (next.equals(neverReach)) {
+					return true;
+				}
+				returnValue = checkForCycles(next, neverReach, graph);
+				if (returnValue) {
+					break;
+				}
 			}
 		}
 		return returnValue;
 	}
 
-	private boolean checkForModule(Long node, Long search) {
-		for (Long next : graph.get(node)) {
-			if (next.equals(search)) {
-				return true;
+	private boolean checkForModule(Long node, Long search, HashMap<Long, List<Long>> graph) {
+		if (graph.get(node) != null) {
+			for (Long next : graph.get(node)) {
+				if (next.equals(search)) {
+					return true;
+				}
 			}
 		}
 		return false;
@@ -70,11 +74,15 @@ public class DependenciesService {
 
 	@Transactional
 	public boolean makeDependent(Long idModuleMain, Long idModuleDependent) {
-		makeGraph(idModuleMain);
+		if (idModuleMain.equals(idModuleDependent)) {
+			// TODO: передавать соответствующее сообщение об ошибке
+			return false;
+		}
 
-		if (!checkForCycles(idModuleMain, idModuleMain)) {
+		HashMap<Long, List<Long>> graph = makeGraph(idModuleMain);
+		if (!checkForCycles(idModuleDependent, idModuleMain, graph)) {
 			if (graph.containsKey(idModuleMain)) {
-				if (!checkForModule(idModuleMain, idModuleDependent)) {
+				if (!checkForModule(idModuleMain, idModuleDependent, graph)) {
 					graph.get(idModuleMain).add(idModuleDependent);
 				} else {
 					// TODO: передавать соответствующее сообщение об ошибке
@@ -107,23 +115,25 @@ public class DependenciesService {
 		return true;
 	}
 
-	private void dfs2(Long takenCourseId, Long node,
-			HashMap<Long, List<ModuleSimpleDto>> newGraph) {
-		for (Long next : graph.get(node)) {
-			dfs2(takenCourseId, next, newGraph);
+	private void dfs2(Long takenCourseId, Long node, HashMap<Long, List<ModuleSimpleDto>> newGraph,
+			HashMap<Long, List<Long>> graph) {
+		if (graph.get(node) != null) {
+			for (Long next : graph.get(node)) {
+				dfs2(takenCourseId, next, newGraph, graph);
+			}
+			List<ModuleSimpleDto> list = graph.get(node).stream()
+					.map(moduleId -> new ModuleSimpleDto(moduleId,
+							moduleRepository.getReferenceById(moduleId).getName(),
+							checkIsOpen(takenCourseId, moduleId)))
+					.toList();
+			newGraph.put(node, list);
 		}
-		List<ModuleSimpleDto> list = graph.get(node).stream()
-				.map(moduleId -> new ModuleSimpleDto(moduleId,
-						moduleRepository.getReferenceById(moduleId).getName(),
-						checkIsOpen(takenCourseId, moduleId)))
-				.toList();
-		newGraph.put(node, list);
 	}
 
 	public HashMap<Long, List<ModuleSimpleDto>> getGraphOfModules(Long takenCourseId, Long root) {
-		makeGraph(root);
+		HashMap<Long, List<Long>> graph = makeGraph(root);
 		HashMap<Long, List<ModuleSimpleDto>> newGraph = new HashMap<>();
-		dfs2(takenCourseId, root, newGraph);
+		dfs2(takenCourseId, root, newGraph, graph);
 		return newGraph;
 	}
 }
